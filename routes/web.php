bc079f1d467a8f6d06d2393e33c3ba344959ef9e
jedkao3851 <sleep6412@gmail.com>
2022-01-20 09:34:04 +0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/detail', function () {
+    return view('detail');
+});
+
+Route::resource('crawler', CrawlerController::class);
